5736d5581f6dc12d2c7de32983b6a37c07a86780
Faiz Elfahad Kurniawan <74404911+siunlucky@users.noreply.github.com>
2024-06-26 13:09:11 +0700

    Setup Vercel
    "

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const home = require('./routes/home')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express()
+app.use(express.json())
+
+app.use('/home', home)
+
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
